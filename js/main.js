41d98a731d547fb81365400e9660ac3f0878eb6f
PetejStone <petejstoney@gmail.com>
2018-02-27 11:36:18 -0800

    fixed carousel bugs and added transitions

--- js/main.js
+++ js/main.js
@@ -32,122 +32,79 @@ function myFunction() {
 
 //Modal Overlay appear on click
 
-for (let i=0; i<card.length; i+=1) {
+for (let i=0; i<card.length; i+=1) { //loops through card list
   card[i].addEventListener('click', (e)=> {
-    counter += i;
+    counter += i;  //counter is set to the index value of the card clicked
     console.log(counter);
-    overlay.style.display = "flex";
-    modalCard[counter].style.display = 'flex';
+    overlay.style.display = "flex";   //when clicked, overlay modal appears
+    modalCard[counter].style.display = 'flex'; //--
 
-    if (modalCard[counter].previousElementSibling !== null) {
+    if (modalCard[counter].previousElementSibling !== null)  { //if a previous sibling exists, display it offscreen to the left
       modalCard[counter].previousElementSibling.style.display = 'flex';
-      modalCard[counter].previousElementSibling.style.transform = 'translate(-175%, -50%)';
+      modalCard[counter].previousElementSibling.style.transform = 'translate(-190%, -50%)'; //left screen placement
+        if (modalCard[counter].previousElementSibling.previousElementSibling !==null) {
+          modalCard[counter].previousElementSibling.previousElementSibling.style.display = 'flex';
+          modalCard[counter].previousElementSibling.previousElementSibling.style.transform = 'translate(-290%, -50%)'; //right screen p
+      }
     }
 
-    if (modalCard[counter].nextElementSibling !== null) {
+
+    if (modalCard[counter].nextElementSibling !== null ) { //if a next sibling exists, display it offscreen to the right
       modalCard[counter].nextElementSibling.style.display = 'flex';
-      modalCard[counter].nextElementSibling.style.transform = 'translate(75%, -50%)';
+      modalCard[counter].nextElementSibling.style.transform = 'translate(90%, -50%)'; //right screen placement
+      if (modalCard[counter].nextElementSibling.nextElementSibling !==null) {
+        modalCard[counter].nextElementSibling.nextElementSibling.style.display = 'flex';
+        modalCard[counter].nextElementSibling.nextElementSibling.style.transform = 'translate(190%, -50%)'; //right screen p
+      }
     }
 
+
+
+
+///////ARROW FUNCTIONALITY///////
     if (i===0) {
-      leftArrow.className = 'disabled';
+      leftArrow.className = 'disabled'; //if the card clicked is the first child, set left arrow to disabled
     }
 
     if (i === card.length -1) {
-      rightArrow.className = 'disabled';
+      rightArrow.className = 'disabled'; // if the card clicked is the last child, set right arrow to disabled
     }
 
 
-
         leftArrow.addEventListener('click', ()=> {
-          rightArrow.className = 'active';
+          rightArrow.className = 'active'; //when the left arrow is clicked, reactivate the right button
           counter += -1;
           console.log(counter);
           if (counter === 0) {
-            leftArrow.className = 'disabled';
+            leftArrow.className = 'disabled'; //if the index value is 0 (the first element), then reset the left arrow to disabled
           }
-          modalCard[counter + 1].nextElementSibling ? modalCard[counter + 1].nextElementSibling.style.display = 'none' : null; //previous right slide disappears
-          modalCard[counter + 1].style.transform = 'translate(75%, -50%)' //current slide move right
-          modalCard[counter].style.transform = 'translate(-50%, -50%)'; //left slide move to center
-          modalCard[counter].previousElementSibling.style.display = 'flex'; //display new left slide
-          modalCard[counter].previousElementSibling.style.transform = 'translate(-175%, -50%)'; //set new left slide to left position
+          modalCard[counter + 1].nextElementSibling ? modalCard[counter + 1].nextElementSibling.style.transform = 'translate(190%, -50%)' : null; //previous right rotates off screen to the right
+          modalCard[counter + 1].style.transform = 'translate(90%, -50%)' //current slide move right
+          modalCard[counter + 1].previousElementSibling.previousElementSibling ? modalCard[counter + 1].previousElementSibling.previousElementSibling.style.transform = 'translate(-190%, -50%)' : null; //two slides previous moves to the partial view on the left
+          modalCard[counter].style.transform = 'translate(-50%, -50%)'; //current left slide move to center
+          modalCard[counter].previousElementSibling.previousElementSibling.style.display = 'flex'; //set new left slide to left position
+          modalCard[counter].previousElementSibling.previousElementSibling.style.transform = 'translate(-290%, -50%)';
 
 
         });
-
-
-
 
 
         rightArrow.addEventListener('click', ()=> {
-          leftArrow.className = 'active';
+          leftArrow.className = 'active';//reactivate left arrow when right arrow is clicked
           counter += 1;
           if (counter === modalCard.length -1) {
-            rightArrow.className = 'disabled';
+            rightArrow.className = 'disabled'; //if the index value is the last child,  then reset the right arrow to disabled
           }
-          modalCard[counter - 1].previousElementSibling ? modalCard[counter - 1].previousElementSibling .style.display = 'none' : null; //previous left slide disappears
-          modalCard[counter - 1].style.transform = 'translate(-175%, -50%)' //current slide move left
+          modalCard[counter - 1].previousElementSibling ? modalCard[counter - 1].previousElementSibling .style.transform = 'translate(-290%, -50%)' : null; //previous left slide disappears only if it exists
+          modalCard[counter - 1].style.transform = 'translate(-190%, -50%)'; //current slide move left
+          modalCard[counter - 1].nextElementSibling.nextElementSibling ? modalCard[counter - 1].nextElementSibling.nextElementSibling.style.transform = 'translate(90%, -50%)' : null;
           modalCard[counter].style.transform = 'translate(-50%, -50%)'; //right slide move to center
-          modalCard[counter].nextElementSibling.style.display = 'flex'; //display new right slide
-          modalCard[counter].nextElementSibling.style.transform = 'translate(75%, -50%)'; //set new right slide to right position
+          modalCard[counter].nextElementSibling.nextElementSibling.style.display = 'flex'; //display new right slide
+          modalCard[counter].nextElementSibling.nextElementSibling.style.transform = 'translate(190%, -50%)'; //set new right slide to right position
 
 
         });
-
-
-
-
-
-
-      // if (modalCard[counter] === modalCard[0]) {
-      //   leftArrow.style.opacity = 0;
-      //   leftArrow.style.cursor = 'default';
-      // }
-
-
-
-      // leftArrow.addEventListener('click', ()=> {
-      //   if (modalCard[counter].nextElementSibling !== null){
-      //       modalCard[counter].nextElementSibling.style.display = 'none';
-      //   }
-      //   if (counter > 0) {
-      //   counter += -1;
-      //   console.log(counter);
-      //   modalCard[counter + 1].style.transform = 'translate(115%, -50%)';
-      //   modalCard[counter + 1].previousElementSibling.style.transform = 'translate(-50%, -50%)';
-      //   modalCard[counter].previousElementSibling.style.display = 'flex';
-      //   modalCard[counter].previousElementSibling.style.transform = 'translate(-215%, -50%)';
-      //
-      //   if (counter < 1) {
-      //     console.log('hello');
-      //   }
-      //   }
-      // });
-
-
-
-
-
-
-
 
 
 });
 }/////End of For Loop
-
-
-
-
-
-
-//-------Modal-Card display and functionality=----
-/////arrow functionality
-
-
-
-
-
-//end of Modal Overlay
-// for (let i=0; i<modalCard.length; i+=1) {
-//   modalCard[i].style.display ="flex";
-// }
